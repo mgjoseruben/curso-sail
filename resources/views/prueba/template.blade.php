678122b6b77488b1eff067bcb1617c205ca8f350
José Rubén Mariño García <mgjoseruben@gmail.com>
2023-10-07 13:55:23 +0200

    template

---
+++ resources/views/prueba/template.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="container mx-auto">
+        Este es el template de prueba.
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/prueba', function () {
+    return view('prueba.template');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
